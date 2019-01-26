e15e9bf41e65657796d39b3cd4088dcafdc856a7
John Barksdale <jdbarks@gmail.com>
2019-01-25 21:13:55 -0600

    Created different creature classes.  Finished the tutorial.

--- actors.py
+++ actors.py
@@ -1,16 +1,25 @@
 import random
 
 
-class Wizard:
-    def __init__(self, name, level):
+class Creature:
+    def __init__(self, name, the_level):
         self.name = name
-        self.level = level
+        self.level = the_level
+
+    def __repr__(self):
+        return f'Creature {self.name} of level {self.level}'
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Wizard(Creature):
 
     def attack(self, creature):
         print(f'The wizard {self.name} attacks {creature.name}!')
 
-        my_roll = random.randint(1, 12) * self.level
-        creature_roll = random.randint(1, 12) * creature.level
+        my_roll = self.get_defensive_roll()
+        creature_roll = creature.get_defensive_roll()
 
         print(f'You roll {my_roll}...')
         print(f'{creature.name} rolls {creature_roll}...')
@@ -22,10 +31,22 @@ class Wizard:
             print("The wizard has been DEFEATED")
             return False
 
-class Creature:
-    def __init__(self, name, the_level):
-        self.name = name
-        self.level = the_level
 
-    def __repr__(self):
-        return f'Creature {self.name} of level {self.level}'
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
+        self.breaths_fire = breaths_fire
+        self.scaliness = scaliness
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        fire_modifier = 5 if self.breaths_fire else 1
+        scale_modifier = self.scaliness /10
+
+        return base_roll * fire_modifier * scale_modifier
